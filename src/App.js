a88a847416e8edd7727ad68cd010858fb4123e33
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2025-05-24 18:43:34 +0600

    landing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import TopNav from './components/TopNav/TopNav';
 import Nav from './components/Nav/Nav';
+import Landing from './components/TopNav/Landing/Landing';
 
 function App() {
   return (
     <div className=" ">
        <TopNav></TopNav>
        <Nav></Nav>
+       <Landing></Landing>
     </div>
   );
 }
